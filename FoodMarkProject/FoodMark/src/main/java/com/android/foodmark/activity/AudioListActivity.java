f31e12be3010e8129c016653a721ff015baa4006
libin <android.bensin@gmail.com>
2013-12-19 16:50:08 -0500

    play device audios feature
    - read the audio files in the device
    - play the audio using mediaplayer

---
+++ FoodMarkProject/FoodMark/src/main/java/com/android/foodmark/activity/AudioListActivity.java
@@ -0,0 +1,41 @@
+package com.android.foodmark.activity;
+
+import android.os.Bundle;
+import android.view.Menu;
+
+import com.android.foodmark.R;
+import com.android.foodmark.fragment.AudioListFragment;
+
+/**
+ * Created by libin on 12/19/13.
+ */
+public class AudioListActivity extends BaseActivity
+{
+    private AudioListFragment mAudioListFragment = null;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_audio_list);
+
+        if(savedInstanceState == null)
+        {
+            mAudioListFragment = new AudioListFragment();
+            // add the fragment to fragment manager
+            getSupportFragmentManager().beginTransaction().add(
+                    R.id.audio_listframe,mAudioListFragment).commit();
+        }
+        else
+        {
+            mAudioListFragment = (AudioListFragment) getSupportFragmentManager()
+                    .findFragmentById(R.id.audio_listframe);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        return true;
+    }
+}
---
+++ FoodMarkProject/FoodMark/src/main/java/com/android/foodmark/model/AudioDetail.java
@@ -0,0 +1,30 @@
+package com.android.foodmark.model;
+
+/**
+ * Created by libin on 12/19/13.
+ */
+public class AudioDetail
+{
+    private long id;
+    private String title;
+
+    public long getId()
+    {
+        return id;
+    }
+
+    public void setId(long id)
+    {
+        this.id = id;
+    }
+
+    public String getTitle()
+    {
+        return title;
+    }
+
+    public void setTitle(String title)
+    {
+        this.title = title;
+    }
+}
